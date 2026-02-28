03402d0cb93a18923de88d501dcf0cc4447172c6
Asad <muhammadasadk42@gmail.com>
2026-02-28 17:02:17 +0500

    linear and quadratic probing in hash tables

---
+++ Week 5 (HashTables)/Lab Task/linearProbing.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 10;
+
+int HT[SIZE];
+
+void insert(int value){
+    int index = value % SIZE;
+    for (int i = 0; i < SIZE;i++){
+        int pos = (index + i) % SIZE;
+        if (HT[pos] == -1 || HT[pos] == -2){
+            HT[pos] = value;
+            cout << "Linearly inserted " << value << " @ index " << pos << endl;
+            return;
+        }
+
+    }
+    cout << " " << endl;
+}
+
+void search (int value){
+    int index = value % SIZE;
+    for (int i = 0; i < SIZE;i++){
+        int pos = (index + i) % SIZE;
+        
+        if(HT[pos]==-1)
+            break;
+
+        if (HT[pos]==value){
+            cout << "Found " << value << " @ index " << pos << endl;
+            return;
+        }    
+    }
+    cout << value << " not found" << endl;
+}
+
+void display(){
+    for (int i = 0; i < SIZE;i++){
+        cout << i << ":";
+        if(HT[i]==-1)
+            cout << "Empty";
+
+        else if(HT[i]==-2)
+            cout << "Deleted";
+
+        else
+            cout << HT[i];
+
+        cout << endl;
+    }
+}
+
+int main() {
+    for (int i = 0; i < SIZE; i++) HT[i] = -1;
+
+    insert(5); insert(15); insert(25);
+    display();
+    search(15);
+    
+    return 0;
+}
+
+
